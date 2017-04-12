9ddf7789298815bb83cd129c559026a4d593238a
Sam Almeida <samRAlmeida@icloud.com>
2017-04-12 16:36:43 -0400

    number 2

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,11 +1,15 @@
 // Exercise 1
-var age = 32;
-var weight = 170;
-result = (age + weight);
-var firstName = "Sam";
-var lastName = "Almeida";
-nameResult = (firstName + " " + lastName);
-console.log();
+// var age = 32;
+// var weight = 170;
+// result = (age + weight);
+// var firstName = "Sam";
+// var lastName = "Almeida";
+// nameResult = (firstName + " " + lastName);
+// console.log();
 
 // Exercise 2
+// var states = [['Delaware','Florida'],['Pensylvania','California']];
+// console.log(states[0][1]);
+
+// Exercise 3
 
