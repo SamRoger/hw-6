c90f5ea8707f8ca16dc00976a4464fa0200b30f1
Sam Almeida <samRAlmeida@icloud.com>
2017-04-12 16:23:23 -0400

    first exercise

---
+++ assets/js/main.js
@@ -0,0 +1,11 @@
+// Exercise 1
+var age = 32;
+var weight = 170;
+result = (age + weight);
+var firstName = "Sam";
+var lastName = "Almeida";
+nameResult = (firstName + " " + lastName);
+console.log();
+
+// Exercise 2
+
